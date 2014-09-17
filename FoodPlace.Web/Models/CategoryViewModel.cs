adef3fd6a94e4a4f2bf498479e3246130a60db33
Dobri <dobromir.v.zlatkov@gmail.com>
2014-09-18 00:06:33 +0300

    categories controller

---
+++ FoodPlace.Web/Models/CategoryViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace FoodPlace.Web.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Linq.Expressions;
+
+    using FoodPlace.Models;
+
+    public class CategoryViewModel
+    {
+        public static Expression<Func<Category, CategoryViewModel>> FromCategory
+        {
+            get
+            {
+                return category => new CategoryViewModel
+                {
+                    Id = category.Id,
+                    Name = category.Name,
+                };
+            }
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; }      
+    }
+}
