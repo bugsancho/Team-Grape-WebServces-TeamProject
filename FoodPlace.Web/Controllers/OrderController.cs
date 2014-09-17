9cdfe1e1b1a7ae5cce21469754fcda386197d21e
bugsancho <bugsancho@gmail.com>
2014-09-17 11:33:04 +0300

    Added an extra controller for testing purposes

---
+++ FoodPlace.Web/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FoodPlace.Data;
+using FoodPlace.Models;
+using System.Web.Http;
+
+namespace FoodPlace.Web.Controllers
+{
+    public class OrderController : BaseApiController
+    {
+         public OrderController() :this(new FoodPlaceData( new FoodPlaceDbContext()))
+        {
+
+        }
+        public OrderController(IFoodPlaceData data) : base(data)
+        {
+        }
+         [HttpGet]
+        public IHttpActionResult Get()
+        {
+            var product = new Order() { Id = 5, TimeOfOrder = DateTime.Now };
+          
+            return Ok(product);
+        }
+    }
+}
