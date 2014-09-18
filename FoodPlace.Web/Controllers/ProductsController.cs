ecf969d72b26b0ce7da71847432d082effbfe475
georgikostadinov <georgikostadinov@yahoo.com>
2014-09-18 14:03:31 +0300

    Added services to delete product and change product's name, price, description, category, picture or size

--- FoodPlace.Web/Controllers/ProductsController.cs
+++ FoodPlace.Web/Controllers/ProductsController.cs
@@ -80,5 +80,142 @@
 
             return Ok(products);
         }
+
+        [HttpPost]
+        public IHttpActionResult CreateProduct(ProductModel product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Can't get the category without making a request to the db, because the model uses string for Category
+            // The product model does not have pictureUrl it can't be added to the database
+            Product newProduct = new Product()
+            {
+                Description = product.Description,
+                Name = product.Name,
+                Category = this.data.Categories.All().Where(c => c.Name == product).FirstOrDefault(),
+                Price = product.Price,
+                Size = product.Size,
+                SizeUnit = (SizeUnit)Enum.Parse(typeof(SizeUnit), product.SizeUnit)                 
+            };
+
+            this.data.Products.Add(newProduct);
+            this.data.SaveChanges();
+
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public IHttpActionResult ChangeProductName(int id, string newName)
+        {
+            var oldProductQuery = this.data.Products.All().Where(p => p.Id == id);
+            if (oldProductQuery == null)
+            {
+                return NotFound();
+            }
+
+            oldProductQuery.FirstOrDefault().Name = newName;
+            this.data.SaveChanges();
+
+            return Ok(oldProductQuery.Select(ProductModel.FromProduct));
+        }
+
+        [HttpPut]
+        public IHttpActionResult ChangeProductCategory(int id, int newCategoryId)
+        {
+            var productQuery = this.data.Products.All().Where(p => p.Id == id);
+            if (productQuery == null)
+            {
+                return NotFound();
+            }
+
+            var category = this.data.Categories.All().Where(c => c.Id == newCategoryId).FirstOrDefault();
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            productQuery.FirstOrDefault().Category = category;
+            this.data.SaveChanges();
+            
+            return Ok(productQuery.Select(ProductModel.FromProduct));
+        }
+
+        [HttpPut]
+        public IHttpActionResult ChangeProductDescription(int id, string newDescription)
+        {
+            var oldProductQuery = this.data.Products.All().Where(p => p.Id == id);
+            if (oldProductQuery == null)
+            {
+                return NotFound();
+            }
+
+            oldProductQuery.FirstOrDefault().Description = newDescription;
+            this.data.SaveChanges();
+
+            return Ok(oldProductQuery.Select(ProductModel.FromProduct));
+        }
+
+        [HttpPut]
+        public IHttpActionResult ChangeProductPrice(int id, decimal newPrice)
+        {
+            var oldProductQuery = this.data.Products.All().Where(p => p.Id == id);
+            if (oldProductQuery == null)
+            {
+                return NotFound();
+            }
+
+            oldProductQuery.FirstOrDefault().Price = newPrice;
+            this.data.SaveChanges();
+
+            return Ok(oldProductQuery.Select(ProductModel.FromProduct));
+        }
+
+        [HttpPut]
+        public IHttpActionResult ChangeProductPicture(int id, string newPictureUrl)
+        {
+            var oldProductQuery = this.data.Products.All().Where(p => p.Id == id);
+            if (oldProductQuery == null)
+            {
+                return NotFound();
+            }
+
+            oldProductQuery.FirstOrDefault().PictureUrl = newPictureUrl;
+            this.data.SaveChanges();
+
+            return Ok(oldProductQuery.Select(ProductModel.FromProduct));
+        }
+
+        [HttpPut]
+        public IHttpActionResult ChangeProductSize(int id, int newSize)
+        {
+            var oldProductQuery = this.data.Products.All().Where(p => p.Id == id);
+            if (oldProductQuery == null)
+            {
+                return NotFound();
+            }
+
+            oldProductQuery.FirstOrDefault().Size = newSize;
+            this.data.SaveChanges();
+
+            return Ok(oldProductQuery.Select(ProductModel.FromProduct));
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteProduct(int id)
+        {
+            var product = this.data.Products.All().Where(p => p.Id == id).FirstOrDefault();
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            this.data.Products.Delete(product);
+            this.data.SaveChanges();
+
+            return Ok(product);
+        }
     }
 }
